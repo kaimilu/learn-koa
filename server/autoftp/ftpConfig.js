86126cf7a40946ec9b9b1fec79e72d6a6c1cb614
suzhihui <wisdom.su@163.com>
2017-07-05 18:26:19 +0800

    add ftp

---
+++ server/autoftp/ftpConfig.js
@@ -0,0 +1,14 @@
+let config = {
+  ftp: {
+    host: '112.74.28.***',
+    port: 657,
+    user: '**',
+    password: '**123'
+  },
+  localDir: '',
+  server: 'zczj.com',
+  svnUrl: 'https://realand-zxn/svn/code/',
+  uploadDir: ['html', 'static', 'statics', 'skin', 'Views', 'Content']
+}
+
+module.exports = config
